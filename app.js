5b4e678044c5fdc58075d4fe7936de7ec9b55b03
Neev <introvertlearnernb@gmail.com>
2023-11-24 12:44:20 +0545

    again changing variable port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(Express.json());
 
 //configuring port to use environment variable PORT or 5000
 // const port = process.env.PORT || 5000;
-const port = `0.0.0.0:${PORT}`;
+const port = `0.0.0.0:$PORT`;
 
 //Connecting to the database.....
 connectDB();
